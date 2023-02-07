e6f3e07f89d5939c2c61d1cb940b4b66a9266aeb
Birdy Bird <edixon@student.neumont.edu>
2023-02-07 08:06:05 -0700

    Prepared MVC ASP .NET Project for development.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using CharacterCreator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CharacterCreator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Home Page";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            ViewData["Title"] = "Privacy Policy";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
