3e6eef43aad09bdaaf2fb41b395e6c5d46f8e79a
OliverQueenEpicMoments <jguerradominguez@student.neumont.edu>
2023-02-09 19:32:46 -0700

    Added a very basic create account page and an account model.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CharacterCreator.Controllers
+{
+	public class AccountController : Controller
+	{
+		public IActionResult Index() {
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult CreateAccount() {
+			return View();
+		}
+
+		// These paramaters are the old way of doing it, once we get connected to a database I or someone else can update them and the method itself
+		[HttpPost]
+		public IActionResult CreateAccount(string Username, string Password) {
+			if (ModelState.IsValid) {
+				TempData["Success"] = "Account Created";
+			}
+			return View();
+		}
+	}
+}
