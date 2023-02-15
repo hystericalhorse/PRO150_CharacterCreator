aa59d814564d8c0b9f4f1c00e07c8a87caa6353b
JosephPe <josephdperjr@gmail.com>
2023-02-14 18:20:52 -0700

    Character Controller, folder/page and layout change.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,6 +25,8 @@ namespace CharacterCreator.Controllers
             return View();
         }
 
+        
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
