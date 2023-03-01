95ffa79598304b8697cebd422ebad22452ac45d5
Birdy Bird <edixon@student.neumont.edu>
2023-02-28 18:52:57 -0700

    Attribute Editor Functional
    The Attribute Select screen is now implemented with a static character located in the controller.

--- Models/AttributeObject.cs
+++ Models/AttributeObject.cs
@@ -7,6 +7,14 @@ namespace CharacterCreator.Models
 		[Key]
 		public int AttributeID { get; set; }
 
+        public AttributeObject() { }
+        public AttributeObject(string name, int score = 0, int bonus = 0)
+        {
+            this.AttributeName = name;
+            this.Score = (AttributeScore) score;
+            this.AttributeBonus = bonus;
+        }
+
 		public enum AttributeScore
         {
             NEUTRAL, STRONG, WEAK
